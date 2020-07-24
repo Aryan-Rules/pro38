900055c7948f33144b715e83604903d56e890fbc
Aryan-Rules <67318638+Aryan-Rules@users.noreply.github.com>
2020-07-24 21:35:31 +0530

    Add files via upload

---
+++ js/Game.js
@@ -0,0 +1,81 @@
+class Game {
+  constructor(){}
+
+  getState(){
+    var gameStateRef  = database.ref('gameState');
+    gameStateRef.on("value",function(data){
+       gameState = data.val();
+    })
+
+  }
+
+  update(state){
+    database.ref('/').update({
+      gameState: state
+    });
+  }
+
+  async start(){
+    if(gameState === 0){
+      player = new HurdleRacer();
+      var playerCountRef = await database.ref('playerCount').once("value");
+      if(playerCountRef.exists()){
+        playerCount = playerCountRef.val();
+        player.getCount()
+      }
+      form = new Form()
+      form.display();
+    }
+     r1=createSprite(20,600)
+     r1.addImage(r1load)
+     r1.scale=0.5;
+     r2=createSprite(20,800)
+     r2.addImage(r2load)
+     r2.scale=0.5
+    cars=[r1,r2]
+    
+  }
+
+  play(){ 
+    form.hide();
+     HurdleRacer.getPlayerInfo(); 
+     if(allPlayers !== undefined){
+      background("white")
+      image(backgroundImage, 0, -20, displayWidth * 5, displayHeight);
+        var index = 0;
+         var x ;
+          var y=120;
+             for(var plr in allPlayers) {
+               index = index + 1 ;
+               y= y + 300;
+                x = displayWidth+allPlayers[plr].distance;
+                 cars[index-1].x = x; 
+                 cars[index-1].y = y;
+
+               if (index === player.index){ 
+                 fill("green")
+                 ellipse(x,y,70,70)
+                 camera.position.x = cars[index-1].x
+                  camera.position.y = displayWidth/2
+                 }
+                }
+    
+    }
+
+
+    if(keyIsDown(RIGHT_ARROW) && player.index !== null){
+      player.distance +=10
+      player.update();
+      console.log(player.distance)
+    }
+
+    /*if(player.distance===3050){
+      gameState=2
+
+    }*/
+    drawSprites();
+  }
+
+
+  
+}
